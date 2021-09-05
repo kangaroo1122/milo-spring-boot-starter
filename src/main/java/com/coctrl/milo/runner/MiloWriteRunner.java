c5363837d9c6c48c1c9e4bf876874e0706ac4617
胡益 <326170945@qq.com>
2021-09-05 18:33:35 +0800

    写优化

--- src/main/java/com/coctrl/milo/runner/MiloWriteRunner.java
+++ src/main/java/com/coctrl/milo/runner/MiloWriteRunner.java
@@ -1,7 +1,6 @@
 package com.coctrl.milo.runner;
 
 import com.coctrl.milo.model.ReadOrWrite;
-import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
@@ -9,7 +8,6 @@ import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.StatusCode;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -28,18 +26,17 @@ public class MiloWriteRunner {
 
     public boolean run(OpcUaClient opcUaClient) {
         try {
-            List<NodeId> nodeIds = ImmutableList.of(new NodeId(2, entity.getIdentifier()));
+            NodeId nodeId = new NodeId(2, entity.getIdentifier());
 
             Variant variant = new Variant(entity.getValue());
 
             // 不需要写 status 和 timestamps
             DataValue dataValue = new DataValue(variant, null, null);
 
-            CompletableFuture<List<StatusCode>> future = opcUaClient.writeValues(nodeIds, ImmutableList.of(dataValue));
-            List<StatusCode> statusCodes = future.get();
-            StatusCode status = statusCodes.get(0);
+            CompletableFuture<StatusCode> future = opcUaClient.writeValue(nodeId, dataValue);
+            StatusCode status = future.get();
             if (status.isGood()) {
-                log.info("将值 '{}' 写入到点位：{} 成功", variant, nodeIds.get(0));
+                log.info("将值 '{}' 写入到点位：{} 成功", variant, nodeId);
             } else {
                 log.error("点位：{} 写入时出现了异常：{}", entity.getIdentifier(), status);
             }
